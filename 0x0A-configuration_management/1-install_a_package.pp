419493358d527a94d209b01204a6ae21877c6cd5
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-27 14:28:10 +0100

    package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
-# install a package flask
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+# Create a manifest file (e.g., 1-install_a_package.pp)
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
