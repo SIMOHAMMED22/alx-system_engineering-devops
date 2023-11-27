92c2acde19aeb936ad5ab2a0c9f1d594bbbad1ec
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-27 17:53:11 +0100

    package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,8 @@
 # Create a manifest file (e.g., 1-install_a_package.pp)
-
 package { 'python3-pip':
   ensure => 'installed',
 }
-
-package { 'flask':
+age { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
