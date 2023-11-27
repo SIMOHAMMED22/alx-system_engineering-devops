be58094630974640999c5bae639e95b2ffce9a67
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-27 13:54:42 +0100

    package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,4 @@
-# Puppet Manifest for installing Flask version 2.1.0 using pip3
-package { 'python3-pip':
-  ensure => installed,
-}
-
+# install a package flask
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/bin',
-  creates => '/usr/local/lib/python3.8/dist-packages/flask-2.1.0.dist-info',
-  require => Package['python3-pip'],
 }
