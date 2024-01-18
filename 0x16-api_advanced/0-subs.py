52330ea65045668e0d7f23419e4ca9bf581da003
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2024-01-18 10:00:15 -0800

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-""" Fetch the the number of subscribers Reddit API. """
+"""Module that consumes the Reddit API and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "custom"}
+    """return number of subs in an existing account
+    subreddit is the target
+     Args:
+        subreddit (str): subreddit
 
-    response = requests.get(url, headers=headers)
+    Returns:
+        int: number of subscribers
+        """
 
-    if response.status_code == 200:
-        return response.json()["data"]["subscribers"]
-    else:
-        return 0
+    base_url = 'https://www.reddit.com/r/'
+
+    url = '{}{}/about.json'.format(base_url, subreddit)
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    results = requests.get(
+        url,
+        headers=headers,
+        allow_redirects=False
+    )
+    if results.status_code == 200:
+        return results.json()['data']['subscribers']
+    return 0
